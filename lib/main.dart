c4f87fb5991348ad621342e08b12dbd37ef3f40c
JaeHyeonKim19 <jaehyeonkim19@gmail.com>
2025-08-20 17:16:28 +0900

    feat: get location from app

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,37 @@
 import 'package:every_football/web_view_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Permission.location.request();
 
-  runApp(const MyApp());
+  // 위치 권한 요청
+  PermissionStatus status = await Permission.location.request();
+
+  // 권한이 허용된 경우에만 위치 정보 가져오기
+  Position? position;
+  if (status.isGranted) {
+    try {
+      position = await Geolocator.getCurrentPosition();
+    } catch (e) {
+      print('위치 정보를 가져올 수 없습니다: $e');
+    }
+  }
+
+  runApp(MyApp(position: position));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Position? position;
+
+  const MyApp({super.key, this.position});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(scaffoldBackgroundColor: Color(0xff282828)),
-      home: const WebViewScreen(),
+      home: WebViewScreen(position: position),
     );
   }
 }
--- lib/web_view_screen.dart
+++ lib/web_view_screen.dart
@@ -2,9 +2,12 @@ import 'package:every_football/data/const.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:geolocator/geolocator.dart';
 
 class WebViewScreen extends StatefulWidget {
-  const WebViewScreen({super.key});
+  final Position? position;
+
+  const WebViewScreen({super.key, this.position});
 
   @override
   State<WebViewScreen> createState() => _WebViewScreenState();
@@ -13,6 +16,15 @@ class WebViewScreen extends StatefulWidget {
 class _WebViewScreenState extends State<WebViewScreen> {
   final GlobalKey webViewKey = GlobalKey();
   double progress = 0;
+
+  String get _webUrlWithLocation {
+    if (widget.position != null) {
+      final lat = widget.position!.latitude;
+      final lng = widget.position!.longitude;
+      return '$webUrl?lat=$lat&lng=$lng';
+    }
+    return webUrl;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +44,9 @@ class _WebViewScreenState extends State<WebViewScreen> {
                     transparentBackground: true,
                   ),
                   key: webViewKey,
-                  initialUrlRequest: URLRequest(url: WebUri(webUrl)),
+                  initialUrlRequest: URLRequest(
+                    url: WebUri(_webUrlWithLocation),
+                  ),
                 ),
               ),
               Container(
