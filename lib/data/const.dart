7c666656332ec0222c409564ac9d0b560ca63e1d
JaeHyeonKim19 <jaehyeonkim19@gmail.com>
2025-08-27 16:23:34 +0900

    feat: update bgColor

--- lib/data/const.dart
+++ lib/data/const.dart
@@ -3,4 +3,4 @@ import 'package:flutter/material.dart';
 const webUrl = "https://every-football.kr";
 // const webUrl = "http://localhost:3000/";
 
-const bgColor = Color(0xff282828);
+const bgColor = Color(0xff2B2B2B);
